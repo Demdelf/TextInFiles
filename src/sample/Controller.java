59fa4dff95b925586d984b1a8e79d4b33e8755f2
Dmitry Maslov <noignor@mail.ru>
2019-11-25 21:54:44 +0300

    Search and Check by streams

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -8,6 +8,7 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -25,12 +26,19 @@ public class Controller {
     public TreeView tree;
     String path = "C:/logs";
     private String fileType = ".log";
+    String searchedText;
+    @FXML
+    public TextArea text;
+    List<Path> filteredFiles = new ArrayList<>();
 
+
+    public List<Path> getFilteredFiles() {
+        return filteredFiles;
+    }
 
     public String getSearchedText() {
         return searchedText;
     }
-
 
     public String getPath() {
         return path;
@@ -41,22 +49,13 @@ public class Controller {
         currentPath.setText(path);
     }
 
-    private String getFileType() {
+    public String getFileType() {
         return fileType;
     }
     public void setFileType(String type){
         this.fileType = type;
-        currentType.setText("*" + type);
+        currentType.setText(type);
     }
-
-
-
-    //setPath("C:/logs");
-    String searchedText;
-    //= "qwerty";
-
-    @FXML
-    public TextArea text;
 
     @FXML
     public void setText(String s){
@@ -64,13 +63,15 @@ public class Controller {
     }
 
     @FXML
-    private void buttonClicked() {
+    private void buttonClicked()
+    {
+        tree.setRoot(null);
         setTextForSearch();
     }
 
     public void setSearchedText(KeyEvent keyEvent) {
         if(keyEvent.getCode().equals(KeyCode.ENTER)){
-
+            tree.setRoot(null);
             setTextForSearch();
         }
 
@@ -101,6 +102,8 @@ public class Controller {
     public void setTextForSearch(){
         //tree.addEventHandler();
         this.searchedText = textForSearch.getText();
+        filteredFiles = new ArrayList<>();
+        text.setText("");
         fileList = getFileList();
     }
 
@@ -108,23 +111,76 @@ public class Controller {
         text.setText(getStringFromFile(fileList, file));
     }
 
-    private List<Path> getFileList(){
-        SimpleFileTreeItem root = new SimpleFileTreeItem(Paths.get(getPath()));
-        tree.setRoot(root);
-        SearchFileVisitor searchFileVisitor = new SearchFileVisitor();
-        searchFileVisitor.setPartOfContent(getSearchedText());
-        searchFileVisitor.setPartOfName(getFileType());
-        try {
-            Files.walkFileTree(Paths.get(getPath()), searchFileVisitor);
-        } catch (IOException e) {
+    private void getFilteredFiles(File dir){
+        File[] files = null;
+        List<Path> list = new ArrayList<>();
+        try{
+
+                    Files.newDirectoryStream(Paths.get(dir.getCanonicalPath()),
+                            path -> path.toString().endsWith(getFileType())||path.toFile().isDirectory()).forEach(p -> list.add(p));
+        }catch (IOException e) {
             e.printStackTrace();
         }
+        for (Path path1 : list) {
+            System.out.println("**********************");
+            System.out.println(path1);
+            System.out.println("**********************");
+        }
 
-        List<Path> foundFiles = searchFileVisitor.getFoundFiles();
-        if(foundFiles.size() == 0) {
+        for ( Path p : list) {
+            File file = p.toFile();
+            if  (Thread.currentThread().isInterrupted()) {
+                return;
+            }
+            if (!file.isDirectory()) {
+                try {
+                    if (searchText(file, getSearchedText())) {
+                        filteredFiles.add(Paths.get(file.getCanonicalPath()));
+                    }
+                } catch (IOException e) {
+                    System.out.println("Error 1");
+                }
+            } else {
+                getFilteredFiles(file);
+
+                /*if (jCheckBox4.isSelected()) {
+                    findFiles(file);
+                }*/
+            }
+        }
+    }
+
+    private boolean searchText(File file, String string) throws IOException {
+        Path path = Paths.get(file.getCanonicalPath());
+        System.out.println("Searching " + string + " in " + path.toString());
+        boolean result = false;
+        try {
+            result = Files.
+                    lines(path, StandardCharsets.UTF_8).
+                    anyMatch((s) -> s.contains(string));
+        }catch (Exception e){
+            System.out.println("Bad format");
+        }
+
+        System.out.println(result);
+        return result;
+    }
+
+    private List<Path> getFileList(){
+        tree.setRoot(null);
+        Path base = Paths.get(getPath());
+        SimpleFileTreeItem root = new SimpleFileTreeItem(base);
+        tree.setRoot(root);
+
+        getFilteredFiles(base.toFile());
+        if(getFilteredFiles().size() == 0) {
             System.out.println("Ничего не найдено");
+            tree.setRoot(new SimpleFileTreeItem(Paths.get("Didn't find anything")));
             return null;
         }
+        List<Path> foundFiles = getFilteredFiles();
+
+
         if(foundFiles.size() > 1){
             foundFiles.sort((o1, o2) -> o1.toString().length() - o2.toString().length());
         }
@@ -164,7 +220,6 @@ public class Controller {
 
         return foundFiles;
     }
-
 
     private String getStringFromFile(List<Path> foundFiles, Path chosenFile){
         String s = "";
@@ -209,7 +264,6 @@ public class Controller {
 
         return stringBuffer.toString();
     }
-
 
     public void setSearchedFile(MouseEvent mouseEvent) {
         //костыль для получения пути файла из TreeView
